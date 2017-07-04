12dd688d243610aeb2f870c1b640d2380c31ca44
Saptadeep Debnath <saptadeep.deb@gmail.com>
2017-07-04 16:43:25 +0400

    updated with publisher

--- src/obstacle_node/src/obstacle_node.cpp
+++ src/obstacle_node/src/obstacle_node.cpp
@@ -1,7 +1,8 @@
 #include "ros/ros.h"
+#include "std_msgs/UInt8.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/LaserScan.h"
-
+std_msgs::UInt8 msg;
 void scanValues(const sensor_msgs::LaserScan laser)
 {
     //fprintf(stderr,"\n -------- size[%d] ---------- ", (int)laser.ranges.size());
@@ -26,7 +27,10 @@ void scanValues(const sensor_msgs::LaserScan laser)
     }
     if(ang != -1)
     {
-      fprintf(stderr,"\nCLOSEST OBSTACLE AT: %d\n", ang/2);
+      //fprintf(stderr,"\nCLOSEST OBSTACLE AT: %d\n", ang/2);
+      msg.data=ang/2;
+       //obs_pos_pub.publish(msg);
+      
     }
 }
 
@@ -34,10 +38,12 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "obstacle_node");
   ros::NodeHandle n;
+  ros::Publisher obs_pos_pub = n.advertise<std_msgs::UInt8>("obs_pos", 1000);
   ros::Rate r(2);
   while(ros::ok())
   {
     ros::Subscriber hokuyoSubscriber = n.subscribe("/scan", 1, scanValues);
+    obs_pos_pub.publish(msg);
     r.sleep();
   //  ros::Duration(10).sleep();
     ros::spinOnce();
